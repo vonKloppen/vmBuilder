f80ea84b31b8f3a0b0881ba27459b7336ed83c31
vonKloppen <mariusz.nowatzki@gmail.com>
2020-04-20 23:50:34 +0200

    More work

---
+++ conf/vmDefaults.py
@@ -0,0 +1,15 @@
+vmHOSTNAME = 'hostname'
+vmCPU="4"
+vmRAM="4"
+vmDISK="16"
+vmOSTYPE="centos7_64Guest"
+vmVLAN="VLAN"
+vmNOTES="Build on $date by $user"
+
+vmNETWORK="dhcp"
+vmIP="192.168.1.1"
+vmNETMASK="255.255.255.0"
+vmGATEWAY=""
+vmNAMESERVERS="DNS1,DNS2"
+
+vmPurpose="generic"
